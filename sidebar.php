2e69c383f984af5ed5ec1228e0719325de96b295
Mark Skipper <marks@aptivate.org>
2013-10-27 13:48:50 +0000

    Add logo in sidebar
    Removed old sponsors from sidebar
    Use theme options to allow override default logo
    Added Xenonium-logo as default logo

--- sidebar.php
+++ sidebar.php
@@ -9,15 +9,17 @@
 <div id="secondary" class="widget-area grid_3 equal_height" role="complementary">
 
 		<header id="masthead" class="site-header" role="banner">
-			<div class="logo">
-			<h1 class="site-title"><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2><?php bloginfo( 'description' ); ?></h2>
-			</div>
+            <?php 
+                $logo = of_get_option('masthead_logo'); 
+                $alt = of_get_option('masthead_alt_text'); 
+            ?>
+            <a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="<?php echo ($logo); ?>" alt="<?php echo ($alt); ?>"/></a>
 		</header><!-- #masthead .site-header -->
 		
 		<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>	<?php endif; // end sidebar widget area ?>
 		
-		<?php get_template_part( 'sponsors' ); ?>
 		<?php get_search_form(); ?>
+
+        <div class="squarebanner cf"></div>>
 		
 </div><!-- #secondary .widget-area -->
