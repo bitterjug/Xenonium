8ed3a141cdd529f8f698b4873dfd6c3e3d141a6f
Mark Skipper <marks@aptivate.org>
2013-11-20 22:12:14 +0000

    Add option for remotely hosted reports

--- single.php
+++ single.php
@@ -30,21 +30,37 @@ get_header(); ?>
             <?php while(has_sub_field('reports')): ?>
                 <div class="report">
                     <?php 
-                        $report_object = get_sub_field('report');
-                        $url = $report_object["url"];
-                        $title = $report_object["title"];
-                        $cover_object = get_sub_field('cover');
-                        $cover_url = $cover_object["url"];
-                        $cover_alt = $cover_object["alt"];
-                        $alt = empty($cover_alt)? $title : $cover_alt;
-                        $report_caption = $report_object["caption"];
-                        $caption = empty($report_caption)? $title : $report_caption;
-                        $width = 120;
-                        $height = 170;
-                        $cover = aq_resize( $cover_url, $width, $height, true ); //resize & crop thumbnail if necessary
-                        echo "<a href=\"$url\" title=\"$title\">";
-                        echo "<img class=\"report shadow\" src=\"$cover\" alt=\"$alt\"/>";
-                        echo "</a>";
+// The following depends on the custom fields being setup :-
+// 'reports' is repeater containing
+// 'cover', which is the thumbnail image
+// 'location' is either 'local' or 'remote'
+// 'report' is the uploaded report file, IF local
+// 'url' is the address of the remote report if remote
+// caption is the caption to display if remote
+$cover_object = get_sub_field('cover');
+$cover_url = $cover_object["url"];
+$cover_alt = $cover_object["alt"];
+$width = 120;
+$height = 170;
+//resize & crop thumbnail if necessary
+$cover = aq_resize( $cover_url, $width, $height, true ); 
+if (get_sub_field('location')=='local')
+{
+    $report_object = get_sub_field('report');
+    $url = $report_object["url"];
+    $title = $report_object["title"];
+    $alt = empty($cover_alt)? $title : $cover_alt;
+    $report_caption = $report_object["caption"];
+    $caption = empty($report_caption)? $title : $report_caption;
+} else {
+    $url = get_sub_field('url');
+    $caption = get_sub_field('caption');
+    $title = $caption;
+    $alt = $caption;
+}
+echo "<a href=\"$url\" title=\"$title\">";
+echo "<img class=\"report shadow\" src=\"$cover\" alt=\"$alt\"/>";
+echo "</a>";
                     ?>
                     <div class="caption">
                     <?php echo "<a href=\"$url\" title=\"$title\">$caption</a>"; ?>
